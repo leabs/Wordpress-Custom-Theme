ccfb33aa7d2673060475238f21f192d7a902f12e
leabs <stevenleabo@gmail.com>
2019-03-06 07:51:59 -0500

    Setting up required php pages and adding bulma css

---
+++ wp-content/themes/atmosphere/front-page.php
@@ -0,0 +1,14 @@
+<?php get_header();?>
+
+<div class="columns">
+    <div class="column">
+<p>Hey there</p>
+    </div>
+    <div class="column">
+<p>Hey there</p>
+    </div>
+
+</div>
+
+
+<?php get_footer();?>
