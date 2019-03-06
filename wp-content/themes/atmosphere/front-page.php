68defa45a96a0c92db46dbbe25f08a3a6d4bde85
leabs <stevenleabo@gmail.com>
2019-03-06 07:59:56 -0500

    Adding base bulma styles from atmoiot.com

--- wp-content/themes/atmosphere/front-page.php
+++ wp-content/themes/atmosphere/front-page.php
@@ -1,14 +1,317 @@
 <?php get_header();?>
 
-<div class="columns">
-    <div class="column">
-<p>Hey there</p>
+ <!-- Features Section -->
+ <section class="site-section section-focus features-section" id="features-section" style="padding-top:60px; padding-bottom:80px;">
+    <div class="container">
+        <!-- Features -->
+        <div class="columns" style="margin-top:0;">
+            
+            <div class="column has-text-centered">
+                <div style="margin:20px">
+                    <img src="/images/icons/Tools.svg" class="features-icon" alt="Simplify development using a powerful, cloud-based IDE." style="margin: 0 auto;" />
+                </div>
+                <div class=" text-center">
+                    <h4 class="icon-header">Build</h4>
+                    <p>Simplify development using a powerful, cloud-based IDE.</p>
+                </div>
+            </div>
+            
+            <div class="column has-text-centered">
+                <div style="margin:20px">
+                    <img src="/images/icons/Rocket.svg" class="features-icon" alt="Securely provision your device to the cloud." style="margin: 0 auto;" />
+                </div>
+                <div class=" text-center">
+                    <h4 class="icon-header">Connect</h4>
+                    <p>Securely provision your device to the cloud.</p>
+                </div>
+            </div>
+            
+            <div class="column has-text-centered">
+                <div style="margin:20px">
+                    <img src="/images/icons/Admin.svg" class="features-icon" alt="Monitor and manage your devices and data at scale." style="margin: 0 auto;" />
+                </div>
+                <div class=" text-center">
+                    <h4 class="icon-header">Manage</h4>
+                    <p>Monitor and manage your devices and data at scale.</p>
+                </div>
+            </div>
+            
+            <br />
+        </div>
     </div>
-    <div class="column">
-<p>Hey there</p>
+</section> 
+<!-- Features Section -->
+<section class="site-section features-section-no-button">
+    <div class="container">
+        <!-- Section Heading -->
+        <div class="row text-center">
+            <!-- Section Header -->
+            <header class="col-md-12" style="margin-bottom:30px;">
+                <h2 class="section-header has-text-white">Unique triple play capability covers your embedded, mobile, and cloud needs</h2>
+                <br />
+            </header>
+        </div>
+        <!-- Features -->
+        <div class="columns" style="margin-top:0; margin-bottom:40px;">
+            
+            <div class="column has-text-centered">
+                <div style="margin:20px">
+                            <img src="/images/icons/negative/Embedded.svg" class="features-icon" alt="Generate embedded software without being a firmware guru." style="margin: 0 auto;" />
+                       
+                    </div>
+               
+                <div class="text-center">
+                    <h4 class="icon-header-light">Embedded</h4>
+                    <p>Generate embedded software without being a firmware guru.</p>
+                </div>
+            </div>
+            
+            <div class="column has-text-centered">
+                <div style="margin:20px">
+                            <img src="/images/icons/negative/Mobile.svg" class="features-icon" alt="Create Android and iOS apps without all the cost and hassle." style="margin: 0 auto;" />
+                       
+                    </div>
+               
+                <div class="text-center">
+                    <h4 class="icon-header-light">Mobile</h4>
+                    <p>Create Android and iOS apps without all the cost and hassle.</p>
+                </div>
+            </div>
+            
+            <div class="column has-text-centered">
+                <div style="margin:20px">
+                            <img src="/images/icons/negative/UpToCloud.svg" class="features-icon" alt="Easily customize dashboards, notifications, devices and user settings." style="margin: 0 auto;" />
+                       
+                    </div>
+               
+                <div class="text-center">
+                    <h4 class="icon-header-light">Web</h4>
+                    <p>Easily customize dashboards, notifications, devices and user settings.</p>
+                </div>
+            </div>
+            
+            <br>
+            <br>
+        </div>
+        <div class="has-text-centered">
+        <button type="button" class=" btn btn-default atmo-button" onclick="location.href='/platform'">
+                Learn More
+            </button>
+        </div>
     </div>
+</section>
+<section class="site-section section-focus homepage-partners" style="padding-bottom:0; background:#f5f5f5;">
+    <div class="container">
+        <!-- Section Heading -->
+        <div class="row padding-mobile">
+            <!-- Section Content -->
+            <div class="section-content partner-images-homepage">
+                <h3 class="text-center section-subheader" style="color:#9E9E9E">Atmosphere works with best-of-breed companies in IoT</h3>
+                <div class="columns has-text-centered owl-carousel is-hidden-mobile" style="margin:40px 0;">
+                    <div class="column v-center" style="vertical-align: middle; display:inline-block;">
+                        <img src="images/partners/alt/AmazonWebServicesLogo.svg" class="band-one-icon img-responsive" alt="Amazon Web Services Logo" />
+                    </div>
+                    <div class="column v-center" style="vertical-align: middle; display:inline-block;">
+                        <img src="images/partners/alt/AMSLogo.svg" class="band-one-icon img-responsive" alt="AMS Logo" />
+                    </div>
+                    <div class="column v-center" style="vertical-align: middle; display:inline-block;">
+                        <img src="images/partners/alt/ArduinoLogo.svg" class="band-one-icon img-responsive" alt="Arduino Logo" />
+                    </div>
+                    <div class="column v-center" style="vertical-align: middle; display:inline-block;">
+                        <img src="images/partners/alt/BoschLogo.svg" class="band-one-icon img-responsive" alt="Bosch Logo" />
+                    </div>
+                    <div class="column v-center" style="vertical-align: middle; display:inline-block;">
+                        <img src="images/partners/alt/Digi-KeyLogo.svg" class="band-one-icon img-responsive" alt="Digi-Key Logo" />
+                    </div>
 
+                    <div class="column v-center" style="vertical-align: middle; display:inline-block;">
+                        <img src="images/partners/alt/EspressifLogo.svg" class="band-one-icon img-responsive" alt="Espressif Logo" />
+                    </div>
+                    
+                    <div class="column v-center" style="vertical-align: middle; display:inline-block;">
+                        <img src="images/partners/alt/HoneywellLogo.svg" class="band-one-icon img-responsive" alt="Honeywell Logo" />
+                    </div>
+                    <div class="column v-center" style="vertical-align: middle; display:inline-block;">
+                        <img src="images/partners/alt/MikroElektronikaLogo.svg" class="band-one-icon img-responsive" alt="Mikro Elektronika Logo " />
+                    </div>
+                    
+                    <div class="column v-center" style="vertical-align: middle; display:inline-block;">
+                        <img src="images/partners/alt/NXPSemiconductorsLogo.svg" class="band-one-icon img-responsive" alt="NXP Semiconductors Logo" />
+                    </div>
+                   
+                    <div class="column v-center" style="vertical-align: middle; display:inline-block;">
+                        <img src="images/partners/alt/STMicroelectronicsLogo.svg" class="band-one-icon img-responsive" alt="ST Microelectronics Logo" />
+                    </div>
+                    <div class="column v-center" style="vertical-align: middle; display:inline-block;">
+                        <img src="images/partners/alt/TexasInstrumentsLogo.svg" class="band-one-icon img-responsive" alt="Texas Instruments Logo" />
+                    </div>
+                    
+            </div>
+
+            <div class="columns is-hidden-tablet" style="padding-bottom:48px;">
+                    <div class="column is-12 v-center" style="vertical-align: middle; display:block; margin: 10px auto; max-width:70%;">
+                        <img src="images/partners/alt/AmazonWebServicesLogo.svg" class="band-one-icon img-responsive" alt="Amazon Web Services Logo" />
+                    </div>
+                    <div class="column is-12 v-center" style="vertical-align: middle; display:block; margin: 10px auto; max-width:70%;">
+                        <img src="images/partners/alt/Digi-KeyLogo.svg" class="band-one-icon img-responsive" alt="Digi-Key Logo" />
+                    </div>
+                    <div class="column is-12 v-center" style="vertical-align: middle; display:block; margin: 10px auto; max-width:70%;">
+                        <img src="images/partners/alt/EspressifLogo.svg" class="band-one-icon img-responsive" alt="Espressif Logo" />
+                    </div>
+                    <div class="column is-12 v-center" style="vertical-align: middle; display:block; margin: 10px auto; max-width:70%;">
+                        <img src="images/partners/alt/NXPSemiconductorsLogo.svg" class="band-one-icon img-responsive" alt="NXP Semiconductors Logo" />
+                    </div>
+                    <div class="column is-12 v-center" style="vertical-align: middle; display:block; margin: 10px auto; max-width:70%;">
+                        <img src="images/partners/alt/STMicroelectronicsLogo.svg" class="band-one-icon img-responsive" alt="ST Microelectronics Logo" />
+                    </div>
+
+            </div>
+        </div>
+    
 </div>
+</section>
+<!-- Blog Section -->
+<section class="site-section blog-section" id="news">
+    <!-- Section Heading -->
+    <div class="container">
+        <div class="row text-center">
+            <!-- Section Header -->
+            <header class="col-md-8 col-md-push-2 col-sm-12 section-header">
+                <h2 class="section-header">Atmosphere Blog</h2>
+                <br>
+                <p>Keep up with the latest industry insights and platform updates.</p>
+                <br>
+            </header>
+            <!-- Section Content -->
+            <div class="col-md-8 col-md-push-2 col-sm-12 section-content">
+                <p></p>
+            </div>
+        </div>
+        <!-- Blog Articles Container -->
+        <div class="row blog-articles-container" style="margin-top:0;">
+            <!-- Jekyll LOOP -->
+            
+            <!-- Blog Article -->
+            <div class="col-md-4 col-md-push-0 col-sm-10 col-sm-push-1 col-xs-10 col-xs-push-1">
+                <article class="blog-article clearfix">
+                    <!-- Article Header -->
+                    <header class="article-header">
+                        <div class="img-wrap">
+                            <a href="/blog/platform-1-2-3.html"><img data-src="images/blog/PlatformAnnouncement.png" class="lazyload" alt="Article Blog Image"></a>
+                        </div>
+                        <div class="article-title">
+                            <h4 class="title blog-section-title " style="margin-bottom:0;"><a href="/blog/platform-1-2-3.html" style="
+    font-size: 14px;
+    line-height: 20px; font-weight:500;">Welcome to Atmosphere Pro</a></h4>
+                            <!--<h5 class="category">Blog</h5>-->
+                        </div>
+                    </header>
+                    <!-- Article Content -->
+                    <div class="article-content">
+                        <div class="article-date" style="display: block;
+    font-size: 12px !important;
+    color: #9e9e9e; padding:12px;padding-left:0;padding-top:6px;">
+                            <p class="blog-meta">
+                        <span style="text-transform: capitalize;">Platform</span>
+                        <span> | </span><span>Jan</span> <span>14</span><span>, 20</span><span>19</span></p>
+                        </div>
+                        <div class="article-text">
+                            The 1.2.3 platform update welcomes Atmosphere's highly-anticipated Professional version, offering users an upgrade path to connect more devices, add cloud storage, and more features! <a href="/blog/platform-1-2-3.html" class="dark-link" style="text-decoration: underline;">Read more...</a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            
+            <!-- Blog Article -->
+            <div class="col-md-4 col-md-push-0 col-sm-10 col-sm-push-1 col-xs-10 col-xs-push-1">
+                <article class="blog-article clearfix">
+                    <!-- Article Header -->
+                    <header class="article-header">
+                        <div class="img-wrap">
+                            <a href="/blog/edge-vs-cloud.html"><img data-src="images/blog/edge-vs-cloud.jpg" class="lazyload" alt="Article Blog Image"></a>
+                        </div>
+                        <div class="article-title">
+                            <h4 class="title blog-section-title " style="margin-bottom:0;"><a href="/blog/edge-vs-cloud.html" style="
+    font-size: 14px;
+    line-height: 20px; font-weight:500;">Here Comes the Edge (Again)</a></h4>
+                            <!--<h5 class="category">Blog</h5>-->
+                        </div>
+                    </header>
+                    <!-- Article Content -->
+                    <div class="article-content">
+                        <div class="article-date" style="display: block;
+    font-size: 12px !important;
+    color: #9e9e9e; padding:12px;padding-left:0;padding-top:6px;">
+                            <p class="blog-meta">
+                        <span style="text-transform: capitalize;">Insights</span>
+                        <span> | </span><span>Jan</span> <span>08</span><span>, 20</span><span>19</span></p>
+                        </div>
+                        <div class="article-text">
+                            The edge is gaining favor in IoT by enabling options for faster, cheaper, and more secure deployments. <a href="/blog/edge-vs-cloud.html" class="dark-link" style="text-decoration: underline;">Read more...</a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            
+            <!-- Blog Article -->
+            <div class="col-md-4 col-md-push-0 col-sm-10 col-sm-push-1 col-xs-10 col-xs-push-1">
+                <article class="blog-article clearfix">
+                    <!-- Article Header -->
+                    <header class="article-header">
+                        <div class="img-wrap">
+                            <a href="/blog/IoT-data-architecture.html"><img data-src="images/blog/data_architecture_industrial.png" class="lazyload" alt="Article Blog Image"></a>
+                        </div>
+                        <div class="article-title">
+                            <h4 class="title blog-section-title " style="margin-bottom:0;"><a href="/blog/IoT-data-architecture.html" style="
+    font-size: 14px;
+    line-height: 20px; font-weight:500;">IoT System Architectures: Begin with the Data in Mind</a></h4>
+                            <!--<h5 class="category">Blog</h5>-->
+                        </div>
+                    </header>
+                    <!-- Article Content -->
+                    <div class="article-content">
+                        <div class="article-date" style="display: block;
+    font-size: 12px !important;
+    color: #9e9e9e; padding:12px;padding-left:0;padding-top:6px;">
+                            <p class="blog-meta">
+                        <span style="text-transform: capitalize;">Insights</span>
+                        <span> | </span><span>Dec</span> <span>17</span><span>, 20</span><span>18</span></p>
+                        </div>
+                        <div class="article-text">
+                            The data architecture piece of your IoT system architecture should cover your unique needs for data aggregation, filtering, processing and analysis. Depending on the scope of your project, these functions could be small or large, and could exist on the edge, offsite, or in the cloud. <a href="/blog/IoT-data-architecture.html" class="dark-link" style="text-decoration: underline;">Read more...</a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            
+            <!-- /LOOP-->
+        </div>
+        <!-- View All Posts CTA -->
+        <div class="row view-posts text-center">
+            <button type="button" class=" btn btn-default atmo-button-dark" onclick="location.href='/blog/index.html'">
+                View All Posts
+            </button>
+        </div>
+    </div>
+</section> 
+<!-- Contact Section -->
+<section class="site-section-small"  style="background:#00ACC1">
+    <div class="container">
+        <!-- Section Heading -->
+        <div class="text-center has-text-white">
+            <h3 class="section-subheader has-text-white">Send your next product to the cloud</h3>
+            
+            <p>Turn your IoT concept into a connected product reality with Atmosphere.</p>
+            <br>
+            <div class="row buttons-header">
+                
+                    <button type="button" class=" btn btn-default atmo-button atmo-button-primary" onclick=" window.open('https://bit.ly/2q8RO4s','_blank')">
+                Get Started
+            </button>
+            </div>
+        </div>
+    </div>
+</section> 
 
 
 <?php get_footer();?>
--- wp-content/themes/atmosphere/functions.php
+++ wp-content/themes/atmosphere/functions.php
@@ -2,9 +2,17 @@
 function load_stylesheets()
 {
 
-    wp_register_style('stylesheet', get_template_directory_uri() . '/css/bulma.min.css', array(), false, 'all');
+    wp_register_style('bulma', get_template_directory_uri() . '/css/bulma.min.css', array(), false, 'all');
 
-    wp_enqueue_style('stylesheet');
+    wp_enqueue_style('bulma');
+
+    wp_register_style('base-styles', get_template_directory_uri() . '/css/base-styles.css', array(), false, 'all');
+
+    wp_enqueue_style('base-styles');
+
+    wp_register_style('styles', get_template_directory_uri() . '/css/styles.css', array(), false, 'all');
+
+    wp_enqueue_style('styles');
 
 }
 
